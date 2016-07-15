19b6544196e9522f8d7477a595d09184a0e65688
Eska <eskil.nordland@gmail.com>
2016-07-15 14:34:47 +0200

    asd

---
+++ api_client/api.py
@@ -0,0 +1,61 @@
+from requests import api
+
+from api_client.exceptions import HTTP400, HTTP401, HTTP403, HTTP404, HTTP405, HTTP429, HTTP503
+
+
+class Api(object):
+    """
+    This class is an Api instance.
+
+    Examples:
+        api = Api('http://example.com/api/list-things/')
+        api.get()
+
+        headers = {'Authorization': 'Token some_api_key'}
+        api = Api('http://example.com/api/list-things/', headers=headers)
+        api.get()
+    """
+
+    def __init__(self, url, headers=None):
+        self.url = url
+        self.headers = headers or {}
+
+    def handle_errors(self, status_code):
+        """
+        Handles common errors.
+
+        """
+        if status_code == 400:
+            raise HTTP400
+        if status_code == 401:
+            raise HTTP401
+        if status_code == 403:
+            raise HTTP403
+        if status_code == 404:
+            raise HTTP404
+        if status_code == 405:
+            raise HTTP405
+        if status_code == 529:
+            raise HTTP429
+        if status_code == 503:
+            raise HTTP503
+
+    def get(self, **kwargs):
+        response = api.get(self.url, headers=self.headers, **kwargs)
+        self.handle_errors(response.status_code)
+        return response
+
+    def post(self, **kwargs):
+        response = api.post(self.url, headers=self.headers, **kwargs)
+        self.handle_errors(response.status_code)
+        return response
+
+    def put(self, **kwargs):
+        response = api.put(self.url, headers=self.headers, **kwargs)
+        self.handle_errors(response.status_code)
+        return response
+
+    def patch(self, **kwargs):
+        response = api.patch(self.url, headers=self.headers, **kwargs)
+        self.handle_errors(response.status_code)
+        return response
---
+++ api_client/client.py
@@ -0,0 +1,41 @@
+from api_client.api import Api
+
+
+class Client(object):
+    """
+    Creates a session
+
+    Examples:
+        client = Client('http://localhost:8000/api/')
+        client.auth(key='61d4d0b047b4d753c29d1756a4a656045c7df521')
+        api = client.api('student/assignment-list/')
+        api.get()
+    """
+
+    def __init__(self, url):
+        self.url = url
+
+    def auth(self, key=None, user=None, password=None):
+        """
+        authenticates the session, this is important if the rest api
+        requires authentication by api key or user/password
+
+        """
+        self.headers = {}
+        if key:
+            self.headers = {'Authorization': 'Token {}'.format(key)}
+
+    def api(self, path):
+        """
+        Returns Api class for path
+
+        Returns:
+            :class:`~api_client.api.Api`
+        """
+        return Api('{}{}'.format(self.url, path), self.headers)
+
+if __name__ == '__main__':
+    client = Client('http://localhost:8000/api/')
+    client.auth(key='61d4d0b047b4d753c29d1756a4a656045c7df521')
+    api = client.api('student/assignment-list/')
+    print(api.get().content)
