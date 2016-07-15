bf2fdc6d712cc86b543cd26a3f14f9dea004a398
Eska <eskil.nordland@gmail.com>
2016-07-15 14:50:30 +0200

    setup.py added

---
+++ setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup, find_packages
+
+setup(name='devilry-terminal-client',
+      author=(u'Eskil Opdahl Nordland'),
+      author_email='devilry-contact@googlegroups.com',
+      url='http://devilry.org',
+      description='Library for interacting with devilry REST api and cli',
+      packages=find_packages(),
+      include_package_data=True,
+      install_requires=[
+          'setuptools',
+          'requests==2.10.0',
+      ]
+      )
