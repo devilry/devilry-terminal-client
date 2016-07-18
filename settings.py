7c46d2e797bd9ba994cf7f840bb11b2c898f7e79
Eska <eskil.nordland@gmail.com>
2016-07-18 03:38:22 +0200

    Added settings and removed devil.py

---
+++ settings.py
@@ -0,0 +1,11 @@
+
+DEV = True
+
+if DEV:
+    # developement settings
+
+    API_URL = 'http://localhost:8000/api/'
+else:
+    # production settings
+
+    API_URL = 'https://devilry.ifi.uio.no/api/'
