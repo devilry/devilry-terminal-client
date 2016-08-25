b9323e2f1c4a3730c85016da302ae53e310cbe46
Eska <eskil.nordland@gmail.com>
2016-08-26 00:17:09 +0200

    craft_queryparam

--- devilry/devilry_api/assignment.py
+++ devilry/devilry_api/assignment.py
@@ -7,6 +7,12 @@ from devilry.devilry_api.base import BaseAPi
 class Assignment(BaseAPi):
 
     url = '/assignment/'
+    query_params = ['ordering',
+                    'search',
+                    'period_short_name',
+                    'subject_short_name',
+                    'short_name',
+                    'id']
 
     def __init__(self, key, action=None, role=None, **kwargs):
         # check role
@@ -15,11 +21,12 @@ class Assignment(BaseAPi):
 
         self.client = Client(API_URL)
         self.client.auth(key=key)
+        self.role = role
         if action == 'list':
-            self.list(role=role, **kwargs)
+            self.list(**kwargs)
 
-    def list(self, role=None, **kwargs):
-        api = self.client.api('{}{}'.format(self.url, role))
+    def list(self, **kwargs):
+        api = self.client.api('{}{}'.format(self.url, self.role))
         self.result = api.get(**kwargs)
 
     def pretty_print(self):
--- devilry/devilry_api/base.py
+++ devilry/devilry_api/base.py
@@ -1,8 +1,19 @@
 
 class BaseAPi(object):
 
+    @property
+    def query_params(self):
+        raise NotImplementedError('please implement query_params Example query_params = [\'search\', \'ordering\']')
+
     def pretty_print(self):
         raise NotImplementedError('please implement pretty_print function')
 
     def get_json(self):
         raise NotImplementedError('please implement get_json function')
+
+    def craft_queryparam(self, **kwargs):
+        q_param = ''
+        for key, value in kwargs.items():
+            if key in self.query_params:
+                q_param += '&{}={}'.format(key, value)
+        return '/?{}'.format(q_param[1:])
